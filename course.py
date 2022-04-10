66c6a9d55de0ff51a3bae271c24f91e2207b18f9
Ahmad Abbas <hamada.a.ab.02@gmail.com>
2022-04-10 10:42:59 +0300

    Update course.py

--- course.py
+++ course.py
@@ -1,13 +1,6 @@
-import time
-
 
 class Course:
     def __init__(self, name, date, time):
         self.name = name
         self.date = date
         self.time = time
-
-    def __repr__(self):
-        date_string = time.strftime("%d/%m/%Y", self.date)
-        time_string = time.strftime("%H:%M", self.time)
-        return '{' + self.name + ', ' + date_string + ', ' + time_string + '}'
